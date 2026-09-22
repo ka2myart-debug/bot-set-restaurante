c596d66bfd3cd9f242dc59a36163a6b8bd978c10
ka2myart-debug <ka2myart@gmail.com>
2026-09-22 00:34:26 -0300

    Refactor bot code and add HTTP server

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot está online!');
+}).listen(process.env.PORT || 3000);
 require('dotenv').config();
 const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 
